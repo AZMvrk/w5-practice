66404ad995c30756cf12b362a13631792264662b
Zoltan_Mark_Antal <zotyaa95@gmail.com>
2023-10-27 09:52:27 +0200

    save

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 console.log('loaded')
 
 const rootElement = document.querySelector("#root")
-console.log(rootElement)
-
+console.dir(rootElement)
+const mainElement = document.querySelector("main")
 
 const fetchUrl = async (url) => {
-  const response = await fetch(url)
-  const data = await response.json()
-  return data
-
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
 }
 
 const countryComponent = (country) => `
-  <div class="country">
-    <h2>country name: ${country.name.common}</h2>
-    <h3>country pop: ${country.population}</h3>
-  </div>
+    <div class="country">
+        <h2>country name: ${country.name.common}</h2>
+        <h3>country pop: ${country.population}</h3>
+    </div>
 `
 
-async function init() { 
-    const data = await fetchUrl("https://restcountries.com/v3.1/all")
-    console.log(data)
 
-   data.forEach(country => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)))
+
+const makeDomFromData = (data, rootElement, componentFn) => {
+  data.forEach(value => rootElement.insertAdjacentHTML("beforeend", componentFn(value)))
+}
+
+async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
+    const data = await fetchUrl("https://restcountries.com/v3.1/all")
+
+    makeDomFromData(data, rootElement, countryComponent)
+    
+    //data.forEach(country => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)))
+
+    /* const countryElement = document.querySelector("div.country")
+    console.log(countryElement)
+    countryElement.addEventListener("click", () => {
+        countryElement.classList.toggle("clicked")
+    }) */
+
+    const countryElements = document.querySelectorAll("div.country")
+    countryElements.forEach((countryElement) => {
+        countryElement.addEventListener("click", () => {
+            countryElement.classList.toggle("clicked")
+        })
+    })
 }
 
 init()
-//window.addEventListener("load")
